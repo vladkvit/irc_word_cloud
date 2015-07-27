18b8eb777dad7b99ae5c9db2a69b8f47f0c257b9
Vladimir Kvitnevskiy <vkvitnevski@gmail.com>
2015-07-27 19:48:25 -0400

    Length of 2 seems more fun, given the small amount of source text.

--- markovgen2.py
+++ markovgen2.py
@@ -6,6 +6,6 @@ with open('../formatted.txt', 'r') as f:
 text = text.decode('utf-8').replace(u'\u014c\u0106\u014d','-')
 text = text.lower()
 
-markov_gen = markovgenerator.markovgenerator.MarkovGenerator(text, 200, 3)
+markov_gen = markovgenerator.markovgenerator.MarkovGenerator(text, 200, 2)
 
 print( markov_gen.generate_words() )
