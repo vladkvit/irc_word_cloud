4f7e472d508066929dfef1e3602d05281da91561
Vladimir Kvitnevskiy <vkvitnevski@gmail.com>
2015-07-27 00:50:00 -0400

    Now generates usernames and words sorted by usage.

--- irc_parser.py
+++ irc_parser.py
@@ -1,7 +1,13 @@
-import sys
+﻿import sys
+import operator
 
 f = open('../#unmatlabic.log', 'r', encoding="utf8")
 fout = open( '../formatted.txt', 'w', encoding='utf8' )
+fout2 = open( '../sortedwords.txt', 'w', encoding='utf8' )
+fout3 = open( '../usernames.txt', 'w', encoding='utf8' )
+
+wordcount = dict()
+usernamecount = dict()
 
 for line in f:
 	#%line = line.encode(sys.stdout.encoding, errors='replace')
@@ -17,9 +23,41 @@ for line in f:
 		#includes "start logging" spacing and the *XYZ says lines
 		continue
 
-	fout.writelines( parts[1] )
-	#print( parts[1].encode(sys.stdout.encoding, errors='replace') )
-	#print( parts[1], end='\n' )
+	sentence = parts[1]
+	username = parts[0]
+	username = username.split( '<', 1 )[1]
+
+	if( username in usernamecount) == False:
+		usernamecount[username] = 0
+
+	usernamecount[username] += 1
+
+	fout.writelines( sentence )
+	words = sentence.split( ' ' )
+	for word in words:
+		if word == '':
+			continue
+		word = word.strip('\n')
+		word = word.lower()
+
+		if (word in wordcount) == False:
+			wordcount[word] = 0
+		wordcount[word] += 1
+
+	#print( sentence.encode(sys.stdout.encoding, errors='replace') )
+	#print( sentence, end='\n' )
 
 f.close()
 fout.close()
+
+sorted_wordcount = sorted(wordcount.items(), key=operator.itemgetter(1), reverse=True)
+sorted_usernames = sorted(usernamecount.items(), key=operator.itemgetter(1), reverse=True)
+
+for item in sorted_wordcount:
+    fout2.writelines( "{} {}\n".format( item[1], item[0]) )
+
+fout2.close()
+
+for item in sorted_usernames:
+    fout3.writelines(  "{} {}\n".format( item[1], item[0]) )
+fout3.close()
